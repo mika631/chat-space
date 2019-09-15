018b885b2e7ecdc7c180b1bda2b22ff816bf0a02
mika631 <kikijiji336@gmail.com>
2019-09-15 21:36:35 +0900

    make side_bar ,routing

--- db/migrate/20190914024255_create_group_users.rb
+++ db/migrate/20190914024255_create_group_users.rb
@@ -1,8 +1,8 @@
 class CreateGroupUsers < ActiveRecord::Migration[5.0]
   def change
-    create_table :groups_users do |t|
-      t.references :user, null:false, foreign_key: true
+    create_table :group_users do |t|
       t.references :group, null:false, foreign_key: true 
+      t.references :user, null:false, foreign_key: true
       t.timestamps
     end
   end
