abad972d362b2be77f460b85d7eee4d70c1347e8
mika631 <kikijiji336@gmail.com>
2019-09-20 20:07:36 +0900

    remake message.js(if message.image)
    if文を、三項演算子に書き換えた

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,7 @@
 $(function(){
   function buildHTML(message){
-    if ( message.image ) {
+    let image = message.image ? message.image :'';
+
       var html =
         `<div class="message" data-message-id=${message.id}>
             <div class="message__update-info">
@@ -16,29 +17,11 @@ $(function(){
                 ${message.message}
               </p>
             </div>
-              <img src=${message.image} >
+              <img src=${image} >
             </div>`
           return html;
-    } else {
-      var html =
-        `<div class="message" data-message-id=${message.id}>
-            <div class="upper-message">
-              <div class="upper-message__user-name">
-                ${message.user_name}
-              </div>
-              <div class="upper-message__date">
-                ${message.date}
-              </div>
-            </div>
-            <div class="lower-message">
-              <p class="lower-message__content">
-                ${message.content}
-              </p>
-            </div>
-          </div>`
-        return html;
-  };
-}
+    } 
+    
   $('#new_message').on('submit',function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -52,10 +35,13 @@ $(function(){
       contentType: false
     })
     .done(function(data){
+      console.log(data)
       var html = buildHTML(data);
+      $('.messages').append(html);  
+      $('form')[0].reset();
     })
     .fail(function(){
       alert('error');
     })
-  });
+  })
 });
