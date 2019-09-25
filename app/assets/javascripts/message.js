7c450476ac907e121ee2aa186431c383ac6cdf90
mika631 <kikijiji336@gmail.com>
2019-09-25 14:53:40 +0900

    make_automatic_reload
    1. 表示されているメッセージ・現在表示されているグループにdataでid情報を追加。
    >カスタムデータ属性の設定(data)を利用
    2. メッセージを更新するためのアクション(json形式のレスポンスに対するアクション）を実装
    > contorollers>api>messages_controller.rbにindexアクションを記述。
    >.whereで条件指定したデータを取得。
    3. 追加したアクションを動かすためのルーティングを実装
    >controllers/api/messages_controller.rb
    >apiデイレクトリ内のコントローラーを動かす→namespace :ディレクトリ名 do ~ endを使用
    4. 追加したアクションをリクエストするよう実装
    >app/views/api/messages直下にindex.json.jbuilderを作成。
    5. 取得した最新のメッセージをブラウザのメッセージ一覧に追加
    > message.jsにreloadMessages関数を記述。その中に.doneとfailを記述。
    6. 数秒ごとにリクエストするよう実装
    >setInterval()関数を記述
    7. メッセージ分だけスクロールするよう実装
    >.animateで記述
    8. メッセージ一覧のページでのみJSが動くよう実装
    > メッセージ一覧のURLの時だけreloadMessages関数が動くようif文を記述。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,7 +3,7 @@ $(function(){
     let image = message.image ? message.image :'';
 
       var html =
-        `<div class="message" data-message-id=${message.id}>
+        `<div class="message" data-id=${message.id}>
             <div class="message__update-info">
               <p class="message__update-info__talker">
                 ${message.user_name}
@@ -46,4 +46,29 @@ $(function(){
     });
     return false;
   });
+
+  group_id = $('.main-header').data('id')
+
+  if (document.URL.match(`/groups/${group_id}/messages`) ){
+    var reloadMessages = function() {
+      last_message_id = $('.message:last').data('id')
+      group_id = $('.main-header').data('id')
+      $.ajax({
+        url: `/groups/${group_id}/api/messages`,
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        var insertHTML = '';
+        messages.forEach(function(message){
+         insertHTML = buildHTML(message);
+          $('.messages').append(insertHTML);
+        });
+      })
+      .fail(function() {
+      });
+    };
+  }
+  setInterval(reloadMessages, 5000);
 });
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,5 +1,5 @@
 json.id  @message.id
 json.user_name  @message.user.name
 json.data  @message.created_at.strftime("%Y年%m月%d日 %H時%M分")
-json.message  @message.message
+json.(@message, :message)
 json.image  @message.image.url
