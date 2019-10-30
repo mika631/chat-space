9ba0e334b385a678e2d9aa47bab826a67d7c1944
mika631 <kikijiji336@gmail.com>
2019-10-30 19:27:15 +0900

    repair

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,9 +1,11 @@
-$(document).on('turbolinks:load',function(){
-  function buildHTML(message){
-    let image = message.image ? message.image :'';
+$(document).on('turbolinks:load', function ()
+{
+  function buildHTML(message)
+  {
+    let image = message.image ? message.image : '';
 
-      var html =
-        `<div class="message" data-id=${message.id}>
+    var html =
+      `<div class="message" data-id=${message.id}>
             <div class="message__update-info">
               <p class="message__update-info__talker">
                 ${message.user_name}
@@ -19,14 +21,15 @@ $(document).on('turbolinks:load',function(){
             </div>
               <img src=${image} >
             </div>`
-          return html;
-    } 
-    
-  $('#new_message').on('submit',function(e){
+    return html;
+  }
+
+  $('#new_message').on('submit', function (e)
+  {
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action');
-   
+
     $.ajax({
       url: url,
       type: "POST",
@@ -35,42 +38,48 @@ $(document).on('turbolinks:load',function(){
       processData: false,
       contentType: false
     })
-    .done(function(data){
-      console.log(data)
-      var html = buildHTML(data);
-      $('.messages').append(html);
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');    
-      $('form')[0].reset();
-    })
-    .fail(function(){
-      alert('error');
-    });
+      .done(function (data)
+      {
+        console.log(data)
+        var html = buildHTML(data);
+        $('.messages').append(html);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight }, 'fast');
+        $('form')[0].reset();
+      })
+      .fail(function ()
+      {
+        alert('error');
+      });
     return false;
   });
 
   group_id = $('.main-header').data('id')
 
-  if (document.URL.match(`/groups/${group_id}/messages`) ){
-    var reloadMessages = function() {
+  if (document.URL.match(`/groups/${group_id}/messages`))
+  {
+    var reloadMessages = function ()
+    {
       last_message_id = $('.message:last').data('id')
       group_id = $('.main-header').data('id')
       $.ajax({
         url: `/groups/${group_id}/api/messages`,
         type: 'get',
         dataType: 'json',
-        data: {id: last_message_id}
+        data: { id: last_message_id }
       })
-      .done(function(messages) {
-        var insertHTML = '';
-        messages.forEach(function(message){
-         insertHTML = buildHTML(message);
-          $('.messages').append(insertHTML);
-          console.log(message)
-          $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast'); 
+        .done(function (messages)
+        {
+          var insertHTML = '';
+          messages.forEach(function (message)
+          {
+            insertHTML = buildHTML(message);
+            $('.messages').append(insertHTML);
+            $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight }, 'fast');
+          });
+        })
+        .fail(function ()
+        {
         });
-      })
-      .fail(function() {
-      });
     };
   }
   setInterval(reloadMessages, 5000);
