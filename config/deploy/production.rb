6886bc78d7c0fe80d7f8ff404486116d7d36bd96
mika631 <kikijiji336@gmail.com>
2019-11-29 20:31:13 +0900

    remake for AWS5

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '52.194.75.140', user: 'ec2-user', roles: %w{app db web}
+server '13.114.190.175', user: 'ec2-user', roles: %w{app db web}
